299679cbf826f074a94fad482015f9f6185274b3
99niccolo99 <71765784+99niccolo99@users.noreply.github.com>
2022-01-23 16:51:14 +0100

    Update LoginDAO.java

--- guardaTv/src/main/java/unisa/is/guardatv/StorageLayer/LoginDAO.java
+++ guardaTv/src/main/java/unisa/is/guardatv/StorageLayer/LoginDAO.java
@@ -26,7 +26,7 @@ public class LoginDAO {
             if (rs.next()) {
                 Login l = new Login();
                 l.setId(rs.getString(1));
-                l.setIdUtente(rs.getInt(2));
+                l.setIdUtente(rs.getString(2));
                 l.setToken(rs.getString(3));
                 l.setTime(rs.getTimestamp(4));
                 return l;
@@ -48,7 +48,7 @@ public class LoginDAO {
                     "INSERT INTO login (id, idUtente, token, time) VALUES(?, ?,?,?)", Statement.RETURN_GENERATED_KEYS);
             String id = UUID.randomUUID().toString();
             ps.setString(1, id);
-            ps.setInt(2, login.getIdUtente());
+            ps.setString(2, login.getIdUtente());
             ps.setString(3, login.getToken());
             ps.setTimestamp(4, login.getTime());
             if (ps.executeUpdate() != 1) {
@@ -68,7 +68,7 @@ public class LoginDAO {
     public void doUpdate(Login login) {
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement("UPDATE login SET idUtente=?, token=?, time=? WHERE id=?");
-            ps.setInt(1, login.getIdUtente());
+            ps.setString(1, login.getIdUtente());
             ps.setString(2, login.getToken());
             ps.setTimestamp(3, login.getTime());
             ps.setString(4, login.getId());
