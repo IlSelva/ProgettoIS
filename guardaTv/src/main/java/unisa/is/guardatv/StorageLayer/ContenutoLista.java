5f4f610cfdbc75aacb22e9ec82e0b4c5330363ee
99niccolo99 <71765784+99niccolo99@users.noreply.github.com>
2022-01-24 12:01:45 +0100

    Update ContenutoLista.java

--- guardaTv/src/main/java/unisa/is/guardatv/StorageLayer/ContenutoLista.java
+++ guardaTv/src/main/java/unisa/is/guardatv/StorageLayer/ContenutoLista.java
@@ -1,4 +1,4 @@
-package StorageLayer;
+package unisa.is.guardatv.StorageLayer;
 
 /**
  * Questa classe modella il concetto "ContenutoLista" di Contenuti appartenenti alla lista di uno specifico utente all'interno del sistema.
