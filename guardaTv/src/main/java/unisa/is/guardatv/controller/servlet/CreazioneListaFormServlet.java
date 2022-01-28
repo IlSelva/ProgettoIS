f11416717fcc9324fcd15239350f5497d4eb6964
Silvio Pastore <silvioppastore@gmail.com>
2022-01-28 21:46:28 +0100

    Create CreazioneListaFormServlet.java

---
+++ guardaTv/src/main/java/unisa/is/guardatv/controller/servlet/CreazioneListaFormServlet.java
@@ -0,0 +1,25 @@
+package unisa.is.guardatv.controller.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/CreazioneNuovaLista")
+public class CreazioneListaFormServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getSession().getAttribute("utente") == null) {
+			throw new MyServletException("Utente non loggato.");
+		}
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/nuovaLista.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+}
