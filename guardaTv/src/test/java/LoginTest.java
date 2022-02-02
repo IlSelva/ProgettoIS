8debc200edd4b35207c15a0f48c978d177a09821
Aless-FG <alessandroricchetti@protonmail.com>
2022-02-02 19:40:40 +0100

    Add files via upload

--- guardaTv/src/test/java/LoginTest.java
+++ guardaTv/src/test/java/LoginTest.java
@@ -1,25 +1,35 @@
 
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 
+import org.junit.rules.ExpectedException;
+import unisa.is.guardatv.StorageLayer.Utente;
 import unisa.is.guardatv.controller.servlet.GestioneUtente.LoginServlet;
+import unisa.is.guardatv.controller.servlet.MyServletException;
+
+import java.io.PrintWriter;
 
 public class LoginTest {
-
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
     private LoginServlet servlet = new LoginServlet();
     private HttpServletRequest request;
     private HttpServletResponse response;
+    private HttpSession session;
 
     @Before
     public void inizializzaMock() {
@@ -30,10 +40,15 @@ public class LoginTest {
 
     @Test
     public void testUtente() throws Exception {
-		when(request.getParameter("email")).thenReturn("emailCiao@email.com");
-		when(request.getParameter("password")).thenReturn("passwordCiao");
-		servlet.doPost(request, response);
-		verify(response).sendRedirect("index.jsp");
+        exceptionRule.expect(MyServletException.class);
+        exceptionRule.expectMessage("email e/o password non validi.");
+        when(request.getParameter("utente")).thenReturn(null);
+        when(request.getParameter("email")).thenReturn("ciao@gmail.com");
+        when(request.getParameter("password")).thenReturn(null);
+        PrintWriter printWriter = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(printWriter);
+        servlet.doPost(request, response);
+        verify(response).sendRedirect("index.jsp");
 		}
 
 
--- guardaTv/src/test/java/RegistrazioneTest.java
+++ guardaTv/src/test/java/RegistrazioneTest.java
@@ -4,8 +4,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import unisa.is.guardatv.controller.servlet.GestioneUtente.RegistrazioneServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,7 +27,8 @@ import unisa.is.guardatv.controller.servlet.GestioneUtente.RegistrazioneServlet;
 import unisa.is.guardatv.controller.servlet.MyServletException;
 
 public class RegistrazioneTest{
-
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
     private RegistrazioneServlet servlet = new RegistrazioneServlet();
     private HttpServletRequest request;
     private HttpServletResponse response;
@@ -34,105 +42,73 @@ public class RegistrazioneTest{
     }
 
     @Test
-    public void test() throws Exception {
-        when(request.getParameter("registrazione")).thenReturn(null);
-        PrintWriter MyWriter = mock(PrintWriter.class);
-        when(response.getWriter()).thenReturn(MyWriter);
-        servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
-    }
-
-    @Test
     public void testEmail() throws Exception {
+        exceptionRule.expect(MyServletException.class);
+        exceptionRule.expectMessage("E-mail non valida");
         when(request.getParameter("registrazione")).thenReturn("registrazione");
-        when(request.getParameter("email")).thenReturn(null);
-        PrintWriter MyWriter = mock(PrintWriter.class);
-        when(response.getWriter()).thenReturn(MyWriter);
-        servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
-    }
-
-
-    @Test
-    public void testPassword() throws Exception {
-        when(request.getParameter("registrazione")).thenReturn("registrazione");
-        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
-        when(request.getParameter("password")).thenReturn(null);
-        PrintWriter MyWriter = mock(PrintWriter.class);
-        when(response.getWriter()).thenReturn(MyWriter);
-        servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
-    }
-
-    @Test
-    public void testPasswordConferma() throws Exception {
-        when(request.getParameter("registrazione")).thenReturn("registrazione");
-        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
-        when(request.getParameter("password")).thenReturn("MarioRossi2000");
-        when(request.getParameter("passwordConferma")).thenReturn(null);
-        PrintWriter MyWriter = mock(PrintWriter.class);
-        when(response.getWriter()).thenReturn(MyWriter);
-        servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
-    }
-
-    @Test
-    public void testUsername() throws Exception {
-        when(request.getParameter("registrazione")).thenReturn("registrazione");
-        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
-        when(request.getParameter("password")).thenReturn("MarioRossi2000");
-        when(request.getParameter("passwordConferma")).thenReturn("MarioRossi2000");
-        when(request.getParameter("nome")).thenReturn(null);
-        PrintWriter MyWriter = mock(PrintWriter.class);
-        when(response.getWriter()).thenReturn(MyWriter);
-        servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
-    }
-
-    @Test
-    public void testDataDiNascita() throws Exception {
-        when(request.getParameter("registrazione")).thenReturn("registrazione");
-        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
+        when(request.getParameter("email")).thenReturn("marioRossi");
         when(request.getParameter("password")).thenReturn("MarioRossi2000");
         when(request.getParameter("passwordConferma")).thenReturn("MarioRossi2000");
         when(request.getParameter("nome")).thenReturn("MarioRossi");
-        when(request.getParameter("dataDiNascita")).thenReturn("null");
-        PrintWriter MyWriter = mock(PrintWriter.class);
-        when(response.getWriter()).thenReturn(MyWriter);
-        servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
-    }
-
-    @Test
-    public void testRegistrazione() throws Exception {
-        when(request.getParameter("registrazione")).thenReturn("registrazione");
-        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
-        when(request.getParameter("password")).thenReturn("MarioRossi2000");
-        when(request.getParameter("passwordConferma")).thenReturn("MarioRossi2000");
-        when(request.getParameter("nome")).thenReturn("MarioRossi");
-        when(request.getParameter("dataDiNascita")).thenReturn("12-12-2000");
+        Date d = new Date(12,12,2000);
+        when(request.getParameter("dataDiNascita")).thenReturn(String.valueOf(d));
         when(request.getSession()).thenReturn(session);
         PrintWriter MyWriter = mock(PrintWriter.class);
         when(response.getWriter()).thenReturn(MyWriter);
         servlet.doPost(request, response);
-        verify(MyWriter).write(BADREQUEST_MESS);
+
     }
 
+    @Test
+    public void testPassword() throws Exception {
+        exceptionRule.expect(MyServletException.class);
+        exceptionRule.expectMessage("Password non valida.");
+        when(request.getParameter("registrazione")).thenReturn("registrazione");
+        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
+        when(request.getParameter("password")).thenReturn("Mario");
+        when(request.getParameter("passwordConferma")).thenReturn("Mario");
+        when(request.getParameter("nome")).thenReturn("MarioRossi");
+        Date d = new Date(12,12,2000);
+        when(request.getParameter("dataDiNascita")).thenReturn(String.valueOf(d));
+        when(request.getSession()).thenReturn(session);
+        PrintWriter MyWriter = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(MyWriter);
+        servlet.doPost(request, response);
+    }
 
+    @Test
+    public void testPasswordConferma() throws Exception {
+        exceptionRule.expect(MyServletException.class);
+        exceptionRule.expectMessage("Password e conferma differenti");
+        when(request.getParameter("registrazione")).thenReturn("registrazione");
+        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
+        when(request.getParameter("password")).thenReturn("MarioRossi2000");
+        when(request.getParameter("passwordConferma")).thenReturn("MarioRossi200");
+        when(request.getParameter("nome")).thenReturn("MarioRossi");
+        Date d = new Date(12,12,2000);
+        when(request.getParameter("dataDiNascita")).thenReturn(String.valueOf(d));
+        when(request.getSession()).thenReturn(session);
+        PrintWriter MyWriter = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(MyWriter);
+        servlet.doPost(request, response);
 
+    }
 
+    @Test
+    public void testUsername() throws Exception {
+        exceptionRule.expect(MyServletException.class);
+        exceptionRule.expectMessage("Username non valido");
+        when(request.getParameter("registrazione")).thenReturn("registrazione");
+        when(request.getParameter("email")).thenReturn("marioRossi@gmail.com");
+        when(request.getParameter("password")).thenReturn("MarioRossi2000");
+        when(request.getParameter("passwordConferma")).thenReturn("MarioRossi2000");
+        when(request.getParameter("nome")).thenReturn("7");
+        Date d = new Date(12,12,2000);
+        when(request.getParameter("dataDiNascita")).thenReturn(String.valueOf(d));
+        when(request.getSession()).thenReturn(session);
+        PrintWriter MyWriter = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(MyWriter);
+        servlet.doPost(request, response);
 
-
-
-
-
-
-
-
-
-    /** messaggio di errore inviato in caso di bad request. **/
-    private static final String BADREQUEST_MESS = "L'operazione richiesta non e' valida.";
-
-    /** messaggio restituito in caso di successo dell'operazione. **/
-    private static final String SUCCESS_MESS = "L'operazione e' avvenuta correttamente.";
+    }
 }
