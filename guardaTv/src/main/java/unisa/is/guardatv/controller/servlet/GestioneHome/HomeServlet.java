d3c6067a1cb9a45e82b0f4d7256f6d91069e7965
Aless-FG <alessandroricchetti@protonmail.com>
2022-02-04 18:36:03 +0100

    aggiunto javadoc

--- guardaTv/src/main/java/unisa/is/guardatv/controller/servlet/GestioneHome/HomeServlet.java
+++ guardaTv/src/main/java/unisa/is/guardatv/controller/servlet/GestioneHome/HomeServlet.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 import java.util.List;
 
 
-
+/**
+ * Questa classe è una Servlet che gestisce la visualizzazione della home
+ */
 @WebServlet(name = "HomeServlet", urlPatterns="", loadOnStartup=1)
 public class HomeServlet extends HttpServlet {
 	private final ContenutoDAO contenutoDAO = new ContenutoDAO();
@@ -25,11 +27,21 @@ public class HomeServlet extends HttpServlet {
 		getServletContext().setAttribute("generi", generi);
 		super.init();
 	}
-	
+	/**
+	 * @param request un oggetto HttpServletRequest che contiene la richiesta che il client invia alla servlet
+	 * @param response un oggetto HttpServletRequest che contiene la risposta che la servlet invia al client
+	 * @throws ServletException se la richiesta POST non può essere gestita
+	 * @throws IOException se un errore di input o output viene rilevato quando la servlet gestisce la richiesta
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
+	/**
+	 * @param request un oggetto HttpServletRequest che contiene la richiesta che il client invia alla servlet
+	 * @param response un oggetto HttpServletRequest che contiene la risposta che la servlet invia al client
+	 * @throws ServletException se la richiesta GET non può essere gestita
+	 * @throws IOException se la richiesta per il GET non può essere gestita
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		List<Contenuto> contenuti = serviceCont.doRetrieveAll(0, 10);
