5eb5804cf7ff1c6b108aac69f4df76a0a507f482
Silvio Pastore <silvioppastore@gmail.com>
2022-02-08 19:16:31 +0100

    Update doGet VisualizzaListaServlet
    Cambiato metodo doGet, richiama doPost

--- guardaTv/src/main/java/unisa/is/guardatv/controller/servlet/VisualizzaListaServlet.java
+++ guardaTv/src/main/java/unisa/is/guardatv/controller/servlet/VisualizzaListaServlet.java
@@ -30,7 +30,7 @@ public class VisualizzaListaServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        this.doPost(request,response);
     }
 
     /**
