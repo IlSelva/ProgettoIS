8542846f1654a39688bb7df724c9ab078f88c720
Aless-FG <alessandroricchetti@protonmail.com>
2022-02-12 12:55:10 +0100

    aggiunto caso di test

--- guardaTv/src/test/java/GestioneRecensione/RimozioneRecensioneTest.java
+++ guardaTv/src/test/java/GestioneRecensione/RimozioneRecensioneTest.java
@@ -15,6 +15,9 @@ import org.junit.Test;
 
 
 import org.junit.rules.ExpectedException;
+import unisa.is.guardatv.StorageLayer.Contenuto;
+import unisa.is.guardatv.StorageLayer.Recensione;
+import unisa.is.guardatv.StorageLayer.RecensioneDAO;
 import unisa.is.guardatv.StorageLayer.Utente;
 import unisa.is.guardatv.controller.servlet.GestioneRecensione.RimozioneRecensioneServlet;
 import unisa.is.guardatv.controller.servlet.MyServletException;
@@ -65,8 +68,21 @@ public class RimozioneRecensioneTest {
         servlet.doPost(request, response);
     }
 
-
-
-
-
+    @Test
+    public void testContenutoNull() throws Exception {
+        Utente utente = new Utente();
+        utente.setEmail("email@gmail.com");
+        utente.setUsername("Username");
+        utente.setAdministrator(true);
+        when(request.getSession()).thenReturn(session);
+        when(request.getSession().getAttribute("utente")).thenReturn(utente);
+        exceptionRule.expect(MyServletException.class);
+        exceptionRule.expectMessage("Contenuto null");
+        Contenuto c = null;
+        when(request.getSession()).thenReturn(session);
+        when(request.getSession().getAttribute("id")).thenReturn(c);
+        PrintWriter printWriter = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(printWriter);
+        servlet.doPost(request, response);
+    }
 }
